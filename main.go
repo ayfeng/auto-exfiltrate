3f8e1bf84983fd1626b5907cd06da0c587186751
Johannes Leppäkorpi <user3610@protonmail.com>
2020-05-27 22:17:19 +0300

    Implementing the ways of fetching
    Signed-off-by: Johannes Leppäkorpi <user3610@protonmail.com>

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"path/filepath"
+	"os"
 )
 
 
@@ -33,18 +34,51 @@ func (d *dataDump) SetRoot(root bool) {
 
 
 func main() {
+	// Create the model, and assign the locations to grab i.e /home, /etc/passwd, *.conf
 	model := dataDump{}
 	directories := []string{
-		"/home",
-		"....",
+		"/home/",
+		"/root/",
+		"/var/www/html",
+		"/var/log/",
+		"/var/mail/",
 	}
-	filetypes := []string{}
+	filetypes := []string{
+		".conf",
+		".ini",
+		".bak",
+	}
 
+
+	fmt.Println("Starting")
 	fetchFiles(directories, filetypes, &model)
 }
 
 func fetchFiles(directories []string, filetypes []string, m *dataDump) {
-	fmt.Println("INSERT HERE")
+	fmt.Println("Fetching directories...")
+	for index, dir := range directories {
+		var amountOfFiles []string
+		err := filepath.Walk(dir,
+			func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+
+			amountOfFiles = append(amountOfFiles, path)
+			// fmt.Println(path, info.Size())
+			return nil
+		})
+		if err != nil {
+			fmt.Printf("Index: %d, Error: %v\n", index, err)
+		}
+
+		fmt.Printf("Index: %d, Files: %d, Directory: %v\n", index, len(amountOfFiles), dir)
+	}
+
+	// fmt.Println("Fetching important files")
+	// for index, file := range filetypes {
+	// 	continue
+	// }
 }
 
 // The full program will stay private and not pushed here, due to the potential of scammed that way
